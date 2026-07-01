89c5140a9aad9e65827be4a245310c4a32b6b1b5
inkOrCloud <inkorcloud@outlook.com>
2026-07-01 15:08:44 +0800

    initial: Flutter project scaffold with dependencies

---
+++ android/app/src/main/kotlin/com/inkorcloud/echo_vault_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.inkorcloud.echo_vault_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_session
+import just_audio
+import shared_preferences_foundation
+import sqlite3_flutter_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+}
